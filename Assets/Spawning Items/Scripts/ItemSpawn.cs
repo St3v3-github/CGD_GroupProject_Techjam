5ab96090a6b92249a7301a2acc02f5c3ca10e80b
Zac <zachery2.pavey@live.uwe.ac.uk>
2023-10-17 15:23:17 +0100

    Added Item Weightings and refactored random generation.

--- Assets/Spawning Items/Scripts/ItemSpawn.cs
+++ Assets/Spawning Items/Scripts/ItemSpawn.cs
@@ -10,7 +10,11 @@ public class ItemSpawn : MonoBehaviour
 
 
     [SerializeField] List<GameObject> SpawnPoints = new List<GameObject>();
+    [SerializeField] int minSpawnPoints = 0;
     [SerializeField] int maxSpawnPoints = 0;
+    [SerializeField] int minElementSpawns = 0;
+    [SerializeField] int minSpellSpawns = 0;
+    
     void Start()
     {
         GenerateSpawnpoints();
@@ -26,18 +30,24 @@ public class ItemSpawn : MonoBehaviour
     public void GenerateSpawnpoints()
     {
         int listsize = SpawnPoints.Count;
+        int randomSpawnCount = 0;
 
-        int [] chosenNumbers = new int[maxSpawnPoints];
+        randomSpawnCount = UnityEngine.Random.Range(minSpawnPoints, maxSpawnPoints+1);
+        Debug.Log("Number of spawn points is: " + randomSpawnCount);
+
+
+
+        int [] chosenNumbers = new int[randomSpawnCount];
 
         for(int i = 0; i < chosenNumbers.Length; i++) 
         {
             chosenNumbers[i] = 0;
         }
 
-        for(int i = 0; i < maxSpawnPoints; i++) 
+        for(int i = 0; i < randomSpawnCount; i++) 
         {
             int randomNumber = 0;
-           randomNumber = UnityEngine.Random.Range(1, listsize);
+           randomNumber = UnityEngine.Random.Range(1, listsize+1);
             Debug.Log("New random number is: " +  randomNumber);
 
 
@@ -69,18 +79,60 @@ public class ItemSpawn : MonoBehaviour
 
 
 
-       
+
+        int availableSpawns = 0;
+        availableSpawns = randomSpawnCount;
+
+
+
+
+        int spawnedElements = 0;
+        int spawnedSpells = 0;
         for (int i = 0; i<chosenNumbers.Length ;i++) 
         {
-            if (i == 0 || i == 1)
+            
+            if(spawnedElements < minElementSpawns)
             {
                 SpawnPoints[chosenNumbers[i] - 1].GetComponent<SpawnItem>().SpawnObject(SpawnItem.Type.Element);
+                availableSpawns--;
+                spawnedElements++;
             }
-            else if(i == 2 || i == 3)
+            else if (spawnedSpells < minSpellSpawns)
             {
                 SpawnPoints[chosenNumbers[i] - 1].GetComponent<SpawnItem>().SpawnObject(SpawnItem.Type.Spell);
+                availableSpawns--;
+                spawnedSpells++;
+            }
+            else if( availableSpawns > 0 )
+            {
+
+                int randomNumber = 0;
+                randomNumber = UnityEngine.Random.Range(0, 2);
+                Debug.Log("The randomly generated value is: " + randomNumber);
+              
+                if(randomNumber == 0 )
+                {
+                    SpawnPoints[chosenNumbers[i] - 1].GetComponent<SpawnItem>().SpawnObject(SpawnItem.Type.Element);
+                    availableSpawns--;
+                    Debug.Log("Randomly chosen an element.");
+                }
+                else if( randomNumber == 1 )
+                {
+                    SpawnPoints[chosenNumbers[i] - 1].GetComponent<SpawnItem>().SpawnObject(SpawnItem.Type.Spell);
+                    availableSpawns--;
+                    Debug.Log("Randomly chosen a spell.");
+
+                }
             }
 
+
+
+
+
+
+
+           
+       
 
            
         }
