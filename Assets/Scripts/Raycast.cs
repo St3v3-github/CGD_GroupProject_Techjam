50fdaefcdd07e568a849c17e0874ceae727562a6
Zac <zachery2.pavey@live.uwe.ac.uk>
2023-10-18 14:34:37 +0100

    Integrated Raycast Item Pickup Into Inventory.

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -11,6 +11,7 @@ public class Raycast : MonoBehaviour
 {
     InputManager inputManager;
     public float range = 5;
+    public GameObject player;
 
     public void Awake()
     {
@@ -20,23 +21,21 @@ public class Raycast : MonoBehaviour
     void Update()
     {
         //if(inputManager.interactInput)    !!Caused an Error
-        {
+        
             Vector3 direction = Vector3.forward;
             Ray ray = new Ray(transform.position, transform.TransformDirection(direction * range));
             if (Physics.Raycast(ray, out RaycastHit hit, range))
             {
-                if(hit.collider.tag == "Item")
-                {
-                    if (hit.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+                    if (hit.collider.gameObject.TryGetComponent(out ItemScript itemScript))
                     {
                         ItemData new_item_data = hit.collider.gameObject.GetComponent<ItemInfo>().GetItemData();
-                        gameObject.GetComponent<Inventory>().addItem(new_item_data);
+                        player.GetComponent<Inventory>().addItem(new_item_data);
 
 
-                        interactObj.Interact();
+                        itemScript.Interact();
                     }
-                }
+                
             }
-        }
+        
     }
 }
