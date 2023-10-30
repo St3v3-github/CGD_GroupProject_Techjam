8ee86aef3c1973c97a4e1912e58287710da10db9
Harry <Harrison2.kent@live.uwe.ac.uk>
2023-10-30 14:45:24 +0000

    Begun Implementing ability to enable/disable shader

---
+++ Assets/Harry/EnableBloodShader.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class EnableBloodShader : MonoBehaviour
+{
+    public Material BloodShader;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
