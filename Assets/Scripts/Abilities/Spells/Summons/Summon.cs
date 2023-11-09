a3e2e2a4f63ce8908b1eda1f483232d6083fca48
Martyn <martyn.elwell@btopenworld.com>
2023-11-09 17:34:01 +0000

    Summons moving mostly

--- Assets/Scripts/Abilities/Spells/Summons/Summon.cs
+++ Assets/Scripts/Abilities/Spells/Summons/Summon.cs
@@ -19,9 +19,9 @@ public class Summon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+        if (Input.GetKeyDown(KeyCode.T))
         {
-            //Spawn();
+            Spawn();
         }
     }
 
--- Assets/Scripts/Abilities/Spells/Summons/SummonedAnimal.cs
+++ Assets/Scripts/Abilities/Spells/Summons/SummonedAnimal.cs
@@ -8,11 +8,44 @@ public class SummonedAnimal : MonoBehaviour
     public ParticleSystem deathParticle;
     public StatusEffect statusEffect;
     public float damage = 30f;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 10f;
+    private GameObject Target;
 
 
     void Start()
     {
         StartCoroutine(timerCoroutine());
+        if (this.tag == "Player1Spell")
+        {
+            Target = FindTarget("Player2");
+        }
+        else if (this.tag == "Player2Spell")
+        {
+           Target = FindTarget("Player1");
+        }
+    }
+
+    void Update()
+    {
+        if (Target != null)
+        {
+            MoveTowardsTarget(Target.transform.position);
+        }
+        else
+        {
+            transform.Translate(transform.forward * moveSpeed * Time.deltaTime);
+
+            if (this.tag == "Player1Spell")
+            {
+                Target = FindTarget("Player2");
+            }
+            else if (this.tag == "Player2Spell")
+            {
+                Target = FindTarget("Player1");
+            }
+
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -21,7 +54,7 @@ public class SummonedAnimal : MonoBehaviour
         animator.SetTrigger("Attack");
 
         // Code to damage player
-        if (other.tag == "player1")
+        if (other.tag == this.tag)
         {
             AttributeManager attributes = other.gameObject.GetComponent<AttributeManager>();
 
@@ -34,9 +67,47 @@ public class SummonedAnimal : MonoBehaviour
             deathParticle.Play(true);
             StartCoroutine(DeathCoroutine());
         }
-
-        
     }
+
+    GameObject FindTarget(string tag)
+    { 
+        GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag(tag);
+        GameObject closestObject = null;
+        float closestDistance = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+
+        foreach (GameObject obj in objectsWithTag)
+        {
+            float distance = Vector3.Distance(obj.transform.position, currentPosition);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestObject = obj;
+            }
+        }
+
+        // Add code to only work when within line o site
+
+        Debug.Log(closestObject.name);
+        return closestObject;
+    }
+
+    void MoveTowardsTarget(Vector3 targetPosition)
+    {
+        // Calculate the direction from the current position to the target position
+        Vector3 direction = targetPosition - transform.position;
+
+        // Calculate the rotation towards the target
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        // Smoothly rotate towards the target using Quaternion.Slerp
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        // Move towards the target position using Vector3.MoveTowards
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+
+
     private IEnumerator DeathCoroutine()
     {
 
