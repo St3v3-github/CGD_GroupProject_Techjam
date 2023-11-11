9425b06e572a81487c283afe0babba468222fd62
Martyn <martyn.elwell@btopenworld.com>
2023-11-11 04:03:16 +0000

    coding on strike

--- Assets/Scripts/Abilities/Spells/AOE_Strike/CastableAOEStrike.cs
+++ Assets/Scripts/Abilities/Spells/AOE_Strike/CastableAOEStrike.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class CastableAOEStrike : MonoBehaviour
 {
     public float attackRadius = 10f;
+    public GameObject projectionPrefab;
     public GameObject particlePrefab;
+    public GameObject projection;
     public Camera playerCamera;
+    public int state = 0;
 
 
     // Start is called before the first frame update
@@ -21,14 +24,58 @@ public class CastableAOEStrike : MonoBehaviour
     {
         Vector3 centre = GetMouseWorldPosition();
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (state == 0)
         {
-           Strike(centre);
 
-           DetectCharacters(centre);
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                projection = Instantiate(projectionPrefab, Vector3.zero, Quaternion.identity);
+                state = 1;
 
-            
+            }
         }
+        else if (state == 1)
+        {
+            // Get the camera's position and forward direction
+            Vector3 cameraPosition = playerCamera.transform.position;
+            Vector3 cameraForward = playerCamera.transform.forward;
+
+            // Create a ray from the camera's position in the forward direction
+            Ray ray = new Ray(cameraPosition, cameraForward);
+            RaycastHit hit;
+
+
+
+            // Check if the ray hits something on the specified layer
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+
+
+                // Move the specified object to the point where the ray hits the ground
+                UpdateProjection(projection, hit.point);
+            }
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                state = 0;
+                
+                Strike(projection.transform.position);
+
+                DetectCharacters(centre);
+
+                Destroy(projection);
+            }
+        }
+
+    }
+
+    void UpdateProjection(GameObject obj, Vector3 targetPosition)
+    {
+        // Ensure the object stays on the ground by setting its y-coordinate to the hit point's y-coordinate
+        targetPosition.y += 0.2f;
+
+        // Set the object's position to the hit point
+        obj.transform.position = targetPosition;
     }
 
     public void Strike(Vector3 centre)
