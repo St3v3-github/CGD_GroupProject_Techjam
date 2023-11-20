e2f16f3cfb1fcfa6e87bab7d2b42738e61841313
George Betts <george2.betts@live.uwe.ac.uk>
2023-11-20 18:30:42 +0000

    added background music

--- Assets/WorkInProgress/George/Audio/AudioManager.cs
+++ Assets/WorkInProgress/George/Audio/AudioManager.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
+    private List<EventInstance> eventInstances;
+
+    private EventInstance musicEventInstance;
+
     public static AudioManager instance { get; private set; }
 
     private void Awake()
@@ -14,10 +19,44 @@ public class AudioManager : MonoBehaviour
             Debug.LogError("Found more than one Audio Manager in the scene. ");
         }
         instance = this;
+
+        eventInstances = new List<EventInstance>();
+    }
+
+    private void Start()
+    {
+        InitializeMusic(FMODEvents.instance.music);
+    }
+
+    private void InitializeMusic(EventReference musicEventReference)
+    {
+        musicEventInstance = CreateEventInstance(musicEventReference);
+        musicEventInstance.start();
     }
 
     public void PlayOneShot(EventReference sound, Vector3 worldPos)
     {
         RuntimeManager.PlayOneShot(sound, worldPos);
     }
+
+    public EventInstance CreateEventInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        eventInstances.Add(eventInstance);
+        return eventInstance;
+    }
+
+    /*private void Cleanup()
+    {
+        foreach (EventInstance eventInstance in eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Cleanup();
+    }*/
 }
--- Assets/WorkInProgress/George/Audio/FMODEvents.cs
+++ Assets/WorkInProgress/George/Audio/FMODEvents.cs
@@ -8,6 +8,9 @@ public class FMODEvents : MonoBehaviour
     [field: Header("Stag SFX")]
     [field: SerializeField] public EventReference stagSound { get; private set; }
 
+    [field: Header("Music")]
+    [field: SerializeField] public EventReference music { get; private set; }
+
     public static FMODEvents instance { get; private set; }
 
     private void Awake()
