1b88acdf8d715382f629f2ac24e92582fe2d9b64
Martyn <martyn.elwell@btopenworld.com>
2023-11-21 03:21:05 +0000

    Map Created

---
+++ Assets/Scenes/Game Scenes/Fighting Arenas/reference.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reference : MonoBehaviour
+{
+    public ParticleSystem nuke;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nuke.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 180 ) { nuke.Play(); }
+    }
+}
