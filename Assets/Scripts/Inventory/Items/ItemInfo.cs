4debaa858ff5229f0dbc690f42f47bc84eee0f59
Steve <steven4.lewis@live.uwe.ac.uk>
2023-11-22 14:50:11 +0000

    Work on Master rune

--- Assets/Scripts/Inventory/Items/ItemInfo.cs
+++ Assets/Scripts/Inventory/Items/ItemInfo.cs
@@ -6,28 +6,38 @@ public class ItemInfo : MonoBehaviour
 {
     // Start is called before the first frame update
 
-   public ItemData item_data;
+    public ItemData item_data;
+    public List<Mesh> meshes = new List<Mesh>();
+    public List<Material> materials = new List<Material>();
 
     void Start()
     {
-        
+        //fill up mesh and material lists
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public int GetWeight()
     {
         return item_data.spawn_weight;
-        
+
     }
-    
-    public ItemData GetItemData() { 
-    
+
+    public ItemData GetItemData()
+    {
+
         return item_data;
+    }
+
+    public void setItemData(ItemData set_to)
+    {
+        item_data = set_to;
+        this.gameObject.GetComponent<MeshFilter>().mesh = meshes[(int)set_to.ID];
+        this.gameObject.GetComponent<Renderer>().material = materials[(int)set_to.ID];
     }
 
 }
