1168bb7d9dd747b7c6dc23d5d86cee8ef8b162bc
Matt <matthew2.summers@live.uwe.ac.uk>
2023-11-28 15:39:23 +0000

    Actually have done the item data and pickup prefabs now

--- Assets/Scripts/Inventory/ItemData.cs
+++ Assets/Scripts/Inventory/ItemData.cs
@@ -19,7 +19,21 @@ public class ItemData : ScriptableObject
         ICEBALL = 2,
         LIGHTNINGBALL = 3, 
         WINDBALL = 4,
-        FIREWALL = 5
+        FIREWALL = 5,
+        ICEWALL = 6,
+        LIGHTNINGWALL = 7,
+        WINDWALL = 8,
+        FIRESUMMON = 9,
+        ICESUMMON = 10,
+        LIGHTNINGSUMMON = 11,
+        WINDSUMMON = 12,
+        FIRESTRIKE = 13,
+        ICESTRIKE = 14,
+        LIGHTNINGSTRIKE = 15,
+        WINDSTRIKE = 16,
+        BLACKHOLE = 17,
+        HEALSPELL = 18,
+        POISONCLOUD = 19
         //More
     }
 
