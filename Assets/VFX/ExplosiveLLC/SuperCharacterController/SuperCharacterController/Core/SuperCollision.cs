1e5add75e0d7fa7e3bc300b1e05ce39e82d47f30
Martyn <martyn.elwell@btopenworld.com>
2023-11-28 14:05:11 +0000

    Animations added

---
+++ Assets/VFX/ExplosiveLLC/SuperCharacterController/SuperCharacterController/Core/SuperCollision.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public struct SuperCollision
+{
+	public CollisionSphere collisionSphere;
+	public SuperCollisionType superCollisionType;
+	public GameObject gameObject;
+	public Vector3 point;
+	public Vector3 normal;
+}
---
+++ Assets/VFX/ExplosiveLLC/Warrior/Code/WarriorInputSytemController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace WarriorAnims
+{
+	/// <summary>
+	/// Placeholder script.  Extract the actual script from the "InputSystem Support - Requires InputSystem Package".
+	/// </summary>
+	public class WarriorInputSystemController:MonoBehaviour
+	{
+		// Placeholder inputs.
+		[HideInInspector] public bool inputAiming;
+		[HideInInspector] public float inputAimVertical = 0;
+		[HideInInspector] public float inputAimHorizontal = 0;
+		[HideInInspector] public bool inputAttack;
+		[HideInInspector] public bool inputAttackMove;
+		[HideInInspector] public bool inputAttackRanged;
+		[HideInInspector] public bool inputAttackSpecial;
+		[HideInInspector] public bool inputBlock;
+		[HideInInspector] public bool inputDeath;
+		[HideInInspector] public bool inputJump;
+		[HideInInspector] public bool inputLightHit;
+		[HideInInspector] public bool inputRoll;
+		[HideInInspector] public bool inputTarget;
+		[HideInInspector] public bool inputTargetKey;
+		[HideInInspector] public float inputHorizontal = 0;
+		[HideInInspector] public float inputVertical = 0;
+		[HideInInspector] public Vector3 moveInput;
+		[HideInInspector] public Vector2 aimInput;
+	}
+}
