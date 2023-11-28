c25103274aac53b03b38fd52edf74021f664e04d
Martyn <martyn.elwell@btopenworld.com>
2023-11-28 15:49:17 +0000

    Spellmanager and more urp textures

--- Assets/VFX/ExplosiveLLC/Editor/SetupInputLayers.cs
+++ Assets/VFX/ExplosiveLLC/Editor/SetupInputLayers.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace WarriorAnims
 {
-	internal class SetupInputLayers:AssetPostprocessor
+	/*internal class SetupInputLayers:AssetPostprocessor
 	{
 		private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
 		{
@@ -19,5 +19,5 @@ namespace WarriorAnims
 		{
 			EditorGUILayout.LabelField("Before attempting to use the Warrior Animation Mecanim Animation Packs, you must first ensure that the layers and inputs are correctly defined.  There are included Input.Presets and TagLayer.Presets which contain all the settings that you can load in.\n \nYou can remove this message by deleting the SetupInputLayers.cs script in the Editor folder.", EditorStyles.wordWrappedLabel);
 		}
-	}
+	}*/
 }
