5f4a41a81dc00dc9ccaa523a64bd14ae1ce466f5
Steve <steven4.lewis@live.uwe.ac.uk>
2023-11-28 17:45:31 +0000

    Moved Scene Transition Logic to on button press in menu

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -8,13 +8,13 @@ public class LevelLoader : MonoBehaviour
     public Animator transition;
     public float transitionTime = 1f;
 
-    void Update()
+/*    void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
             LoadNextScene();
         }
-    }
+    }*/
 
     public void LoadNextScene()
     {
