cabfd54c5f9d5106276e5dbbc0bb467e953af3a9
joshua sorley <joshua2.sorley@live.uwe.ac.uk>
2023-12-01 12:15:16 +0000

    Level Select Scripts

--- Assets/WorkInProgress/Josh/scripts/LevelSelectController.cs
+++ Assets/WorkInProgress/Josh/scripts/LevelSelectController.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
+    
 
 public class LevelSelectController : MonoBehaviour
 {
+    
+    public GameObject Map; 
+    public Button next;
+    public Button Previous;
+    public int MapNumber = 0;
+    public int Numberofmaps;
+    public GameObject[] Levels;
+    public GameObject StartingMap; 
+    int i; 
     // Start is called before the first frame update
     void Start()
     {
+       
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        Map = Levels[MapNumber];
         
     }
+
+   public void NextMap()
+    {
+        if (MapNumber < Numberofmaps)
+        {
+            MapNumber++;
+        }
+        if (MapNumber == Numberofmaps)
+        {
+            MapNumber = 0; 
+        }
+        for (i = 0; i < Numberofmaps; i++)
+        {
+            Levels[i].SetActive(false);
+        }
+        Map.SetActive(true); 
+    }
+    
 }
