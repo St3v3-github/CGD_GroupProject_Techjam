3911ed716f0b625a7800125f10f73a1f4c89ef0e
Awse Noori <Awse2.Noori@live.uwe.ac.uk>
2023-12-07 17:05:55 +0000

    despawnTime set

--- Assets/WorkInProgress/Awse/Scripts/DamageFlyText.cs
+++ Assets/WorkInProgress/Awse/Scripts/DamageFlyText.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DamageFlyText : MonoBehaviour
 {
-    public float despawnTime;
+    public float despawnTime = 2f;
     // Start is called before the first frame update
     void Start()
     {
