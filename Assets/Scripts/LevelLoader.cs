d9be4225043b16753a2f05847d9e0dff1439babd
george <george2.betts@live.uwe.ac.uk>
2024-02-06 15:19:46 +0000

    added ability to load game from menu

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -6,33 +6,41 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     //Public variables to access scene load from any other script - might need in gamne manager?
-    public Animator transition;
-    public float transitionTime = 1f;
+    //public Animator transition;
+    //public float transitionTime = 1f;
+    [SerializeField] public GameObject a;
+    public int levelSelect;
+
 
     //Test on Mouseclick
-/*    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
+    /*    void Update()
         {
-            LoadNextScene();
-        }
-    }*/
+            if(Input.GetMouseButtonDown(0))
+            {
+                LoadNextScene();
+            }
+        }*/
 
     //Function to load the scene
+    public void NextLevel()
+    {
+        levelSelect = a.GetComponent<LevelSelectController>().LevelNum;
+    }
     public void LoadNextScene()
     {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        SceneManager.LoadScene(levelSelect);
     }
 
     //Courotine To delay the Scene Load - Animation has to play first
-    IEnumerator LoadLevel(int levelIndex)
-    {
-        transition.SetTrigger("Start");
+    //IEnumerator LoadLevel(int levelIndex)
+    //{
+        //transition.SetTrigger("Start");
 
-        yield return new WaitForSeconds(transitionTime);
+        //yield return new WaitForSeconds(transitionTime);
 
-        SceneManager.LoadScene(levelIndex);
+        //SceneManager.LoadScene(levelIndex);
         
         //SceneManager.LoadScene("Scene B");   -Can also do this :)
-    }
+    //}
+    
 }
