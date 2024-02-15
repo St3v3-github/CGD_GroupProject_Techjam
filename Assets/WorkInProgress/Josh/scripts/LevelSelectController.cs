2d192831d9c3e100bc9d92369ca9d7092f2bdb5a
Zac <zachery2.pavey@live.uwe.ac.uk>
2024-02-15 14:27:00 +0000

    Updated Prev button and play button.

--- Assets/WorkInProgress/Josh/scripts/LevelSelectController.cs
+++ Assets/WorkInProgress/Josh/scripts/LevelSelectController.cs
@@ -1,58 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
-    
+using UnityEngine.UI;
+
 
 public class LevelSelectController : MonoBehaviour
 {
-    
-    public GameObject Map; 
+
+    public GameObject Map;
     public Button next;
     public Button Previous;
     public int MapNumber = 0;
     public int Numberofmaps;
     public GameObject[] Levels;
-    public GameObject StartingMap; 
-    int i; 
+    public GameObject StartingMap;
+    int i;
     // Start is called before the first frame update
     void Start()
     {
-        Map = Levels[0]; 
+        Map = Levels[0];
         Map.SetActive(true);
-        
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-       
-        
+
+
     }
 
-   public void NextMap()
+    public void NextMap()
     {
-        if (MapNumber < Numberofmaps)
+        MapNumber++;
+        if(MapNumber==Numberofmaps)
         {
-         MapNumber++;
+            MapNumber = 0;
         }
 
-        if (MapNumber == Numberofmaps)
-        {
-            MapNumber++;
-        }
-
-        if (MapNumber > Numberofmaps)
-        {
-            MapNumber = 0; 
-        }
         for (i = 0; i < Numberofmaps; i++)
         {
             Levels[i].SetActive(false);
         }
         Map = Levels[MapNumber];
-        Map.SetActive(true);  
-        
+        Map.SetActive(true);
+
     }
-    
+
+    public void PrevMap()
+    {
+        MapNumber--;
+        if(MapNumber<0)
+        {
+            MapNumber = Numberofmaps-1;
+        }
+
+        for (i = 0; i < Numberofmaps; i++)
+        {
+            Levels[i].SetActive(false);
+        }
+        Map = Levels[MapNumber];
+        Map.SetActive(true);
+    }
+
+    public void CharSelect()
+    {
+        //Transition to character selection
+    }
 }
