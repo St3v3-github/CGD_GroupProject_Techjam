182729cfe851d49c16390c20bb8144b6360eca42
Martyn <martyn.elwell@btopenworld.com>
2024-02-20 16:08:19 +0000

    hit efefcts

--- Assets/Scripts/Abilities/Spells/Projectiles/Projectile.cs
+++ Assets/Scripts/Abilities/Spells/Projectiles/Projectile.cs
@@ -40,7 +40,7 @@ public class Projectile : Spell
         else if(collision.gameObject.tag != source.tag)
         {
             Debug.Log(collision.name);
-            //GameObject impact = Instantiate(hitImpact, transform.position, Quaternion.identity);
+            GameObject impact = Instantiate(hitImpact, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
 
