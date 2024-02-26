32282cbc57143aaeff729e200980db4c517f5430
rosa <rosa2.kay@live.uwe.ac.uk>
2024-02-26 13:34:06 +0000

    scripts clean-up

---
+++ Assets/Scripts/Abilities/Spells/Status Effects/IEffectable.cs
@@ -0,0 +1,7 @@
+public interface IEffectable
+{
+    public void ApplyEffect(StatusEffect_Data _data);
+    public void RemoveEffect();
+
+    public void HandleEffect();
+}
--- Assets/WorkInProgress/Rosa/Scripts/IEffectable.cs
+++
@@ -1,7 +0,0 @@
-public interface IEffectable
-{
-    public void ApplyEffect(StatusEffect_Data _data);
-    public void RemoveEffect();
-
-    public void HandleEffect();
-}
