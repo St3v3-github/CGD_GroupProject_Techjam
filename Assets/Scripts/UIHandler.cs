e70118d0a91b236d59727b797060aab0df5745e8
Josh <joshua2.sorley@live.uwe.ac.uk>
2024-03-11 10:05:02 +0000

    Hit Chain UI

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
+
 public class UIHandler : MonoBehaviour
 {
     [Header("UI Elements")]
@@ -12,7 +13,7 @@ public class UIHandler : MonoBehaviour
     public RawImage SpellUI;
 
     [Header("Colors")]
-    public Color Char; 
+    public Color Char;
     public Color Title;
     public Color Spell;
 
@@ -26,15 +27,24 @@ public class UIHandler : MonoBehaviour
 
     [Header("Ultimates")]
     public GameObject Poision;
-    public bool PoisionBool; 
+    public bool PoisionBool;
     public GameObject Beam;
     public bool BeamBool;
-    public GameObject Healing; 
+    public GameObject Healing;
     public bool HealingBool;
 
     [Header("Player Disconnect")]
     public GameObject PD;
-    public bool Disconect; 
+    public bool Disconect;
+
+    [Header("Hit Chain")]
+    public bool Hit;
+    public bool ChainInEffect;
+    public bool restart;
+    public int hitchain = 0;
+    public RawImage[] Retical;
+    public GameObject ReticalParent;
+    public Animator Anim; 
 
 
 
@@ -46,20 +56,122 @@ public class UIHandler : MonoBehaviour
         SpellUI.color = Spell;
         TitleText.text = TitleName;
         
+     
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-     if (PoisionBool) {Poision.SetActive(true);}
-     if (!PoisionBool) {Poision.SetActive(false);}
-     if (BeamBool) {Beam.SetActive(true);} 
-     if (!BeamBool) {Beam.SetActive(false);} 
-     if (HealingBool) {Healing.SetActive(true);}
-     if (!HealingBool) {Healing.SetActive(false);}
-     if (Disconect) {PD.SetActive(true);}
-     if (!Disconect) { PD.SetActive(false);}
+        if (PoisionBool) { Poision.SetActive(true); }
+        if (!PoisionBool) { Poision.SetActive(false); }
+        if (BeamBool) { Beam.SetActive(true); }
+        if (!BeamBool) { Beam.SetActive(false); }
+        if (HealingBool) { Healing.SetActive(true); }
+        if (!HealingBool) { Healing.SetActive(false); }
+        if (Disconect) { PD.SetActive(true); }
+        if (!Disconect) { PD.SetActive(false); }
+        if (Hit) { Hitchain(); Hit = false; }
+        
     }
+
+    void Hitchain()
+    {
+        if (ChainInEffect) 
+        { 
+            restart = true;
+            hitchain++; 
+          
+        }
+        
+        if (!ChainInEffect) 
+        {
+            hitchain++;
+            ChainInEffect = true;
+            StartCoroutine(Timer());
+        }
+
+        IEnumerator Timer()
+        {
+            yield return new WaitForSeconds(2);
+            if (!restart) { ChainInEffect = false; reset(); }
+            if (restart) { StartCoroutine(Timer()); restart = false; }
+        }
+
+        if (hitchain == 1)
+        {
+            Retical[0].color = new Color (250,0,0,255);
+        }
+
+        if (hitchain == 2)
+        {
+            Retical[1].color = new Color(250, 0, 0, 255);
+        }
+
+        if (hitchain == 3)
+        {
+            Retical[2].color = new Color(250, 0, 0, 255);
+        }
+
+        if (hitchain == 4)
+        {
+            Retical[3].color = new Color(250, 0, 0, 255);
+        }
+
+        if (hitchain == 5)
+        {
+            ReticalParent.transform.Rotate (0,0,45,Space.Self);
+        }
+
+        if (hitchain == 6)
+        {
+            Anim.Play("Testing Something"); 
+        }
+        if (hitchain == 7)
+        {
+            Retical[0].color = Color.blue;
+        }
+
+        if (hitchain == 8)
+        {
+            Retical[1].color = Color.blue;
+        }
+
+        if (hitchain == 9)
+        {
+            Retical[2].color = Color.blue;
+        }
+
+        if (hitchain == 10)
+        {
+            Retical[3].color = Color.blue;
+        }
+
+        if (hitchain == 11)
+        {
+            Anim.Play("Testing Something");
+        }
+
+    }
+
+    void reset()
+    {
+        hitchain = 0;
+        for (int i = 0; i < Retical.Length; i++)
+        {
+            Retical[i].color = Color.gray;
+            
+        }
+        ReticalParent.transform.Rotate(0, 0, -45, Space.Self);
+
+    }
+
+    
+
+   
+
+    
 
 
 }
