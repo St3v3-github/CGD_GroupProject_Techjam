b8e413da6c92befa3994b9a07262f0cbcaccf102
george <george2.betts@live.uwe.ac.uk>
2024-03-18 14:52:38 +0000

    Implemented a timer script and prefab

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Text = TMPro.TextMeshProUGUI;
+
+public class GameTimer : MonoBehaviour
+{
+    public float timeRemaining = 120;
+    public bool timerIsRunning = false;
+    public Text timeText;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
