b8d32b08d02db6691d4dd4166a9eddf7b93eb132
Zsolt <csongor2.horosnyi@live.uwe.ac.uk>
2024-03-26 15:24:47 +0000

    Customization templating, need logic added still

---
+++ Assets/CharacterComponentLister.cs
@@ -0,0 +1,65 @@
+using PsychoticLab;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterComponentLister : MonoBehaviour
+{
+    [Header("Component Lists")]
+    public CharacterComponentGroup lightComponents; 
+    public CharacterComponentGroup mediumComponents;
+    public CharacterComponentGroup heavyComponents;
+    [System.Serializable]
+    public class PreBuiltHead
+    {
+        public GameObject head;
+        public GameObject eyebrow;
+        public GameObject facialHair;
+        public GameObject accessory;
+    }
+    [System.Serializable]
+    public class PreBuiltChest
+    {
+        public GameObject torso;
+        public GameObject armUpperRight;
+        public GameObject armUpperLeft;
+        public GameObject armLowerRight;
+        public GameObject armLowerLeft;
+        public GameObject handRight;
+        public GameObject handLeft;
+        public GameObject shoulderRight;
+        public GameObject shoulderLeft;
+        public GameObject elbowRight;
+        public GameObject elbowLeft;
+        public GameObject backAttachment;
+    }
+    [System.Serializable]
+    public class PreBuiltLegs
+    {
+        public GameObject hips;
+        public GameObject legRight;
+        public GameObject legLeft;
+        public GameObject kneeAttachmentRight;
+        public GameObject kneeAttachmentLeft;
+        public GameObject hipsAttachment;
+    }
+    [System.Serializable]
+    public class CharacterComponentGroup
+    {
+        public List<PreBuiltHead> head;
+        public List<GameObject> hair;
+        public List<PreBuiltChest> chest;
+        public List<PreBuiltLegs> legs;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
