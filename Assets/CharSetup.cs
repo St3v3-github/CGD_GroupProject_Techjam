4db74cbe73e0bf44e1225601ccd3ab6e6cb13de3
Zsolt <csongor2.horosnyi@live.uwe.ac.uk>
2024-03-28 18:25:21 +0000

    Logic almost implemented, controls and interface need fixing

--- Assets/CharSetup.cs
+++ Assets/CharSetup.cs
@@ -1,10 +1,14 @@
 using FMOD;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using static Unity.Burst.Intrinsics.X86.Avx;
+using static UnityEngine.UI.Image;
 
 public class CharSetup : MonoBehaviour
 {
@@ -18,22 +22,41 @@ public class CharSetup : MonoBehaviour
     List<int> playerClassID = new List<int>();
     public GameObject[] players;
     public int[] menuSelections;
+    public CharMenuLevels[] menuLevels;
     public float[] inputClocks;
-    public int maxSelection = 1;
+    public int maxSelection = 3;
     public PlayerInputManager inputManager;
     public bool kwikfix = true;
-
     public bool updateAfterDestroy = false;
-    // Start is called before the first frame update
+    public List<int>[] customisationIDs;
+    public const int CUSTOMISATION_PARTS = 4;
+    public CharacterComponentLister charCompLister;
+
+    public enum CharMenuLevels
+    {
+        CHAR_CLASS = 0,
+        HAIR_CUSTOM,
+        HEAD_CUSTOM,
+        BODY_CUSTOM,
+        LEGS_CUSTOM,
+        MAIN_CONTROLS,
+        LEVEL_MAX
+    }
+
     void Start()
     {
-        
+        customisationIDs = new List<int>[maxPlayers];
         for (int i = 0; i < maxPlayers; i++)
         {
-            //spaceTaken.Add(false);
             playerClassID.Add(0);
+            customisationIDs[i] = new List<int>();
+            for(int j = 0; j < CUSTOMISATION_PARTS; j++)
+            {
+                customisationIDs[i].Add(0);
+            }
         }
         menuSelections = new int[maxPlayers];
+        menuLevels = new CharMenuLevels[maxPlayers];
         inputClocks = new float[maxPlayers];
     }
 
@@ -93,8 +116,11 @@ public class CharSetup : MonoBehaviour
                 componentRegistries[componentRegistry.playerInput.playerIndex] = componentRegistry;
                 componentRegistry.playerScoreInfo.team = componentRegistry.playerInput.playerIndex;
                 componentRegistry.inputManager.enabled = false;
-                componentRegistry.playerInput.actions.FindAction("MenuRight").performed += NextClass;
-                componentRegistry.playerInput.actions.FindAction("MenuLeft").performed += previousClass;
+                componentRegistry.playerInput.actions.FindAction("MenuRight").performed += MenuRight;
+                componentRegistry.playerInput.actions.FindAction("MenuLeft").performed += MenuLeft;
+                componentRegistry.playerInput.actions.FindAction("MenuUp").performed += MenuUp;
+                componentRegistry.playerInput.actions.FindAction("MenuDown").performed += MenuDown;
+                componentRegistry.playerInput.actions.FindAction("MenuExecute").performed += MenuExecute;
                 //REMEMBER TO DISABLE SPELL SYSTEM
                 //componentRegistry.advancedProjectileSystem.enabled = false;
                 componentRegistry.playerCamera.enabled = false;
@@ -111,7 +137,8 @@ public class CharSetup : MonoBehaviour
     /// Currently left and right movement is hard coded to change class. In the future a switch handler can be implemented.
     /// </summary>
     /// <param name="ctx"></param>
-    public void NextClass(InputAction.CallbackContext ctx)
+    
+    public void MenuLeft(InputAction.CallbackContext ctx)
     {
         for (int i = 0; i < maxPlayers; i++)
         {
@@ -122,14 +149,25 @@ public class CharSetup : MonoBehaviour
                     if (playerInputDevice == ctx.control.device && inputClocks[i] <= 0.0f)
                     {
                         inputClocks[i] = inputTime;
-                        NextClass(i);
+                        //NextClass(i);
+                        /*switch(menuLevels[i])
+                        {
+                            case CharMenuLevels.CHAR_CLASS:
+                                break;
+                        }*/
+                        menuSelections[i]--;
+                        if (menuSelections[i] < 0)
+                        {
+                            menuSelections[i] = maxSelection - 1;
+                        }
                         break;
                     }
                 }
             }
         }
     }
-    public void previousClass(InputAction.CallbackContext ctx)
+
+    public void MenuRight(InputAction.CallbackContext ctx)
     {
         for (int i = 0; i < maxPlayers; i++)
         {
@@ -140,11 +178,277 @@ public class CharSetup : MonoBehaviour
                     if (playerInputDevice == ctx.control.device && inputClocks[i] <= 0.0f)
                     {
                         inputClocks[i] = inputTime;
-                        previousClass(i);
+                        menuSelections[i]++;
+                        if (menuSelections[i] == maxSelection)
+                        {
+                            menuSelections[i] = 0;
+                        }
                         break;
                     }
                 }
             }
+        }
+    }
+
+    public void MenuUp(InputAction.CallbackContext ctx)
+    {
+        for (int i = 0; i < maxPlayers; i++)
+        {
+            if (players[i] != null)
+            {
+                foreach (var playerInputDevice in componentRegistries[i].playerInput.devices)
+                {
+                    if (playerInputDevice == ctx.control.device && inputClocks[i] <= 0.0f)
+                    {
+                        inputClocks[i] = inputTime;
+                        menuLevels[i]--;
+                        if (menuLevels[i] < 0)
+                        {
+                            menuLevels[i] = CharMenuLevels.MAIN_CONTROLS;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void MenuDown(InputAction.CallbackContext ctx)
+    {
+        for (int i = 0; i < maxPlayers; i++)
+        {
+            if (players[i] != null)
+            {
+                foreach (var playerInputDevice in componentRegistries[i].playerInput.devices)
+                {
+                    if (playerInputDevice == ctx.control.device && inputClocks[i] <= 0.0f)
+                    {
+                        inputClocks[i] = inputTime;
+                        menuLevels[i]++;
+                        if (menuLevels[i] >= CharMenuLevels.LEVEL_MAX)
+                        {
+                            menuLevels[i] = CharMenuLevels.CHAR_CLASS;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void MenuExecute(InputAction.CallbackContext ctx)
+    {
+        for (int i = 0; i < maxPlayers; i++)
+        {
+            if (players[i] != null)
+            {
+                foreach (var playerInputDevice in componentRegistries[i].playerInput.devices)
+                {
+                    if (playerInputDevice == ctx.control.device && inputClocks[i] <= 0.0f)
+                    {
+                        inputClocks[i] = inputTime;
+                        switch(menuLevels[i]) 
+                        {
+                            case CharMenuLevels.CHAR_CLASS:
+                                switch(menuSelections[i])
+                                {
+                                    case 0:
+                                        previousClass(i);
+                                        break;
+                                    case 1:
+                                        //TODO: Show Class Info
+                                        break;
+                                    case 2:
+                                        NextClass(i);
+                                        break;
+                                }
+                                break;
+                            case CharMenuLevels.HAIR_CUSTOM:
+                                switch (menuSelections[i])
+                                {
+                                    case 0:
+                                        break;
+                                    case 1:
+                                        break;
+                                    case 2:
+                                        break;
+                                }
+                                break;
+                            case CharMenuLevels.HEAD_CUSTOM:
+                                switch (menuSelections[i])
+                                {
+                                    case 0:
+                                        PrevHead(i);
+                                        break;
+                                    case 1:
+                                        //TODO: Colour picker
+                                        break;
+                                    case 2:
+                                        NextHead(i);
+                                        break;
+                                }
+                                break;
+                            case CharMenuLevels.BODY_CUSTOM:
+                                switch (menuSelections[i])
+                                {
+                                    case 0:
+                                        break;
+                                    case 1:
+                                        break;
+                                    case 2:
+                                        break;
+                                }
+                                break;
+                            case CharMenuLevels.LEGS_CUSTOM:
+                                switch (menuSelections[i])
+                                {
+                                    case 0:
+                                        break;
+                                    case 1:
+                                        break;
+                                    case 2:
+                                        break;
+                                }
+                                break;
+                            case CharMenuLevels.MAIN_CONTROLS:
+                                switch (menuSelections[i])
+                                {
+                                    case 0:
+                                        break;
+                                    case 1:
+                                        break;
+                                    case 2:
+                                        break;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void NextHead(int index)
+    {
+        CharacterComponentLister.PreBuiltHead new_head = new CharacterComponentLister.PreBuiltHead();
+        customisationIDs[index][0]++;
+        switch(componentRegistries[index].meshComponentList.meshList)
+        {
+            case 0:
+                if (customisationIDs[index][0] >= charCompLister.lightComponents.head.Count)
+                {
+                    customisationIDs[index][0] = 0;
+                }
+                new_head = charCompLister.lightComponents.head[customisationIDs[index][0]];
+                break;
+            case 1:
+                if (customisationIDs[index][0] >= charCompLister.mediumComponents.head.Count)
+                {
+                    customisationIDs[index][0] = 0;
+                }
+                new_head = charCompLister.mediumComponents.head[customisationIDs[index][0]];
+                break;
+            case 2:
+                if (customisationIDs[index][0] >= charCompLister.heavyComponents.head.Count)
+                {
+                    customisationIDs[index][0] = 0;
+                }
+                new_head = charCompLister.heavyComponents.head[customisationIDs[index][0]];
+                break;
+        }
+        onHeadChange(index, new_head);
+    }
+
+    public void PrevHead(int index)
+    {
+        CharacterComponentLister.PreBuiltHead new_head = new CharacterComponentLister.PreBuiltHead();
+        customisationIDs[index][0]--;
+        switch (componentRegistries[index].meshComponentList.meshList)
+        {
+            case 0:
+                if (customisationIDs[index][0] < 0)
+                {
+                    customisationIDs[index][0] = charCompLister.lightComponents.head.Count - 1;
+                }
+                new_head = charCompLister.lightComponents.head[customisationIDs[index][0]];
+                break;
+            case 1:
+                if (customisationIDs[index][0] < 0)
+                {
+                    customisationIDs[index][0] = charCompLister.mediumComponents.head.Count - 1;
+                }
+                new_head = charCompLister.mediumComponents.head[customisationIDs[index][0]];
+                break;
+            case 2:
+                if (customisationIDs[index][0] < 0)
+                {
+                    customisationIDs[index][0] = charCompLister.heavyComponents.head.Count - 1;
+                }
+                new_head = charCompLister.heavyComponents.head[customisationIDs[index][0]];
+                break;
+        }
+        UnityEngine.Debug.Log(customisationIDs[index][0]);
+        onHeadChange(index, new_head);
+    }
+
+    public void onHeadChange(int index, CharacterComponentLister.PreBuiltHead new_head)
+    {
+        if(new_head.head != null)
+        {
+            componentRegistries[index].meshComponentList.head.sharedMesh = new_head.head.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+            componentRegistries[index].meshComponentList.head.bounds = new_head.head.GetComponent<SkinnedMeshRenderer>().bounds;
+            componentRegistries[index].meshComponentList.head.materials = new_head.head.GetComponent<SkinnedMeshRenderer>().materials;
+            componentRegistries[index].meshComponentList.head.enabled = true;
+        }
+        else
+        {
+            componentRegistries[index].meshComponentList.head.enabled = false;
+        }
+
+        if (new_head.accessory != null)
+        {
+            componentRegistries[index].meshComponentList.headAccessory.sharedMesh = new_head.accessory.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+            componentRegistries[index].meshComponentList.headAccessory.bounds = new_head.accessory.GetComponent<SkinnedMeshRenderer>().bounds;
+            componentRegistries[index].meshComponentList.headAccessory.materials = new_head.accessory.GetComponent<SkinnedMeshRenderer>().materials;
+            componentRegistries[index].meshComponentList.headAccessory.enabled = true;
+        }
+        else
+        {
+            componentRegistries[index].meshComponentList.headAccessory.enabled = false;
+        }
+
+        if (new_head.facialHair != null)
+        {
+            componentRegistries[index].meshComponentList.facialHair.sharedMesh = new_head.facialHair.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+            componentRegistries[index].meshComponentList.facialHair.bounds = new_head.facialHair.GetComponent<SkinnedMeshRenderer>().bounds;
+            componentRegistries[index].meshComponentList.facialHair.materials = new_head.facialHair.GetComponent<SkinnedMeshRenderer>().materials;
+            componentRegistries[index].meshComponentList.facialHair.enabled = true;
+        }
+        else
+        {
+            componentRegistries[index].meshComponentList.facialHair.enabled = false;
+        }
+
+        if (new_head.eyebrow != null)
+        {
+            componentRegistries[index].meshComponentList.eyebrow.sharedMesh = new_head.eyebrow.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+            componentRegistries[index].meshComponentList.eyebrow.bounds = new_head.eyebrow.GetComponent<SkinnedMeshRenderer>().bounds;
+            componentRegistries[index].meshComponentList.eyebrow.materials = new_head.eyebrow.GetComponent<SkinnedMeshRenderer>().materials;
+            componentRegistries[index].meshComponentList.eyebrow.enabled = true;
+        }
+        else
+        {
+            componentRegistries[index].meshComponentList.eyebrow.enabled = false;
+        }
+    }
+
+    public void copyMesh(SkinnedMeshRenderer original, SkinnedMeshRenderer new_mesh)
+    {
+        System.Type type = original.GetType();
+        System.Reflection.FieldInfo[] fields = type.GetFields();
+        foreach (System.Reflection.FieldInfo field in fields)
+        {
+            field.SetValue(original, field.GetValue(new_mesh));
         }
     }
 
@@ -152,17 +456,12 @@ public class CharSetup : MonoBehaviour
     {
         if (kwikfix && componentRegistries[index] != null && players[index] != null)
         {
-            var playerDevice = componentRegistries[index].playerInput.devices.FirstOrDefault<InputDevice>();
-            var playerControlScheme = componentRegistries[index].playerInput.currentControlScheme;
             playerClassID[index]++;
             if (playerClassID[index] == playerClassRotation.Count())
             {
                 playerClassID[index] = 0;
             }
-            players[index].SetActive(false);
-            UnityEngine.Debug.Log("Player " + index.ToString() + " is cycling class...");
-            PlayerInput.Instantiate(playerClassRotation[playerClassID[index]], index, playerControlScheme, index, playerDevice);
-            updateAfterDestroy = true;
+            onClassChange(index);
         }
     }
 
@@ -170,17 +469,26 @@ public class CharSetup : MonoBehaviour
     {
         if (kwikfix && componentRegistries[index] != null && players[index] != null)
         {
-            var playerDevice = componentRegistries[index].playerInput.devices.FirstOrDefault<InputDevice>();
-            var playerControlScheme = componentRegistries[index].playerInput.currentControlScheme;
             playerClassID[index]--;
             if (playerClassID[index] == -1)
             {
                 playerClassID[index] = playerClassRotation.Count()-1;
             }
-            players[index].SetActive(false);
-            UnityEngine.Debug.Log("Player " + index.ToString() + " is cycling class...");
-            PlayerInput.Instantiate(playerClassRotation[playerClassID[index]], index, playerControlScheme, index, playerDevice);
-            updateAfterDestroy = true;
+            onClassChange(index);
+        }
+    }
+
+    public void onClassChange(int index)
+    {
+        var playerDevice = componentRegistries[index].playerInput.devices.FirstOrDefault<InputDevice>();
+        var playerControlScheme = componentRegistries[index].playerInput.currentControlScheme;
+        players[index].SetActive(false);
+        UnityEngine.Debug.Log("Player " + index.ToString() + " is cycling class...");
+        PlayerInput.Instantiate(playerClassRotation[playerClassID[index]], index, playerControlScheme, index, playerDevice);
+        updateAfterDestroy = true;
+        for(int i = 0; i < customisationIDs[index].Count; i++) 
+        {
+            customisationIDs[index][i] = 0;
         }
     }
 }
