105bf3d66e5dd7706f4d09ffe330d1c2fb30825e
Zac <zachery2.pavey@live.uwe.ac.uk>
2024-04-18 16:29:51 +0100

    Setup class select and customisation UI. Needs connecting to logic.

---
+++ Assets/CustomisationUIHandler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomisationUIHandler : MonoBehaviour
+{
+
+    public GameObject[] ClassButtons;
+    public GameObject[] HairButtons;
+    public GameObject[] HeadButtons;
+    public GameObject[] TorsoButtons;
+    public GameObject[] LegButtons;
+    public Material notSelectedMat;
+    public Material selectedMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void UpdateUISeclections(int selectedUI)
+    {
+
+        // SWITCH FOR CURRENT ACTIVE MENU OPTIONS ENUM
+        // DISABLE ALL HIGHLIGHTS AND ENABLE THE SELECTEDUI OPTION'S HIGHLIGHT
+
+
+
+    }
+}
