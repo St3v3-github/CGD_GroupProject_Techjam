7ef092c6c518daea42052351803161bb5616672e
rosa <rosa2.kay@live.uwe.ac.uk>
2024-04-24 14:18:27 +0100

    all effects working & slow & stun implemented -READY FOR MERGE!
    have changed spell prefabs to contain correct status effect data. and have changed player prefabs so camera shake works on all elementals.

--- Assets/Scripts/Abilities/Spells/A. Spell Redesign/Scripts to Revisit/FireGrenade.cs
+++ Assets/Scripts/Abilities/Spells/A. Spell Redesign/Scripts to Revisit/FireGrenade.cs
@@ -11,7 +11,7 @@ public class Grenade : Spell
     private bool spawned = false;
     public float damage;
     public bool effectNeedsDamage = false;
-    //public StatusEffect_Data effect;
+    public StatusEffect_Data effect;
 
     //private float timer = 0;
 
@@ -27,33 +27,60 @@ public class Grenade : Spell
 
     void OnTriggerEnter(Collider collision)
     {
-        if(!spawned && collision.transform.parent != null && !collision.transform.parent.CompareTag("Player"))
-        {
-            spawned = true;
-            GameObject effect = Instantiate(Prefab, transform.position, Quaternion.identity);
-            //effect.GetComponent<Spell>().source = source;
-            
-            if(effectNeedsDamage)
-            {
-                effect.GetComponent<ExplodeAfterDuration>().damage = damage;
-                effect.GetComponent<ExplodeAfterDuration>().source = source;
-                
-            }
-            else
-            {
-                effect.GetComponent<DeleteOnTimer>().setupDelete(activeTime);
-            }
-
-            if(effect.GetComponent<PoisonCloud>() != null)
-            {
-                effect.GetComponent<PoisonCloud>().source = source;
-            }
-            if (effect.GetComponent<FireCircle>() != null)
-            {
-                effect.GetComponent<FireCircle>().source = source;
-            }
-
-            Destroy(gameObject);
+        if(!spawned && collision.transform.parent != null && !collision.transform.parent.CompareTag("Player"))
+
+        {
+
+            spawned = true;
+
+            GameObject effect = Instantiate(Prefab, transform.position, Quaternion.identity);
+
+            //effect.GetComponent<Spell>().source = source;
+
+            
+
+            if(effectNeedsDamage)
+
+            {
+
+                effect.GetComponent<ExplodeAfterDuration>().damage = damage;
+
+                effect.GetComponent<ExplodeAfterDuration>().source = source;
+
+                
+
+            }
+
+            else
+
+            {
+
+                effect.GetComponent<DeleteOnTimer>().setupDelete(activeTime);
+
+            }
+
+
+
+            if(effect.GetComponent<PoisonCloud>() != null)
+
+            {
+
+                effect.GetComponent<PoisonCloud>().source = source;
+
+            }
+
+            if (effect.GetComponent<FireCircle>() != null)
+
+            {
+
+                effect.GetComponent<FireCircle>().source = source;
+
+            }
+
+
+
+            Destroy(gameObject);
+
         }
        /* if (dealDamage(collision.gameObject, DirectHitDamage))
         {
