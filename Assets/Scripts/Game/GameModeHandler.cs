fcfc5670b7bbbca20e01c81178b2b181396f3384
Zac <zachery2.pavey@live.uwe.ac.uk>
2024-04-30 11:40:59 +0100

    Fixed Build Error

--- Assets/Scripts/Game/GameModeHandler.cs
+++ Assets/Scripts/Game/GameModeHandler.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
