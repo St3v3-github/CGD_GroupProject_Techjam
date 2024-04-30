9e2e44fc285100dea6929a7e0afabec20cc63390
Harry <Harrison2.kent@live.uwe.ac.uk>
2024-04-30 12:05:19 +0100

    Fixed Build errors

--- Assets/Scripts/_Player/Sliding.cs
+++ Assets/Scripts/_Player/Sliding.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class Sliding : MonoBehaviour
